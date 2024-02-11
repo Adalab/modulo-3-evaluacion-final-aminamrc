78bea1f330003bb5b6b6b7cca0e1ec2bd7ec5d40
aminamrc <amina.mrc@gmail.com>
2024-02-11 10:00:32 +0100

    btn back added

--- src/components/details/DetailedCharacter.jsx
+++ src/components/details/DetailedCharacter.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 
 function DetailedCharacter({characters}) {
@@ -13,7 +13,10 @@ function DetailedCharacter({characters}) {
   //   return <li className="list-item-style" key={char.id}> {char} 
   //   </li> 
   // })
-  return (
+  return (  <>
+    <div className="btn-style">
+     <Link to="/" className="rtn-style"><p> 🠔 Volver </p>  </Link>
+    </div>
     <div className="card-style-details">
       { idData.image === "" ? <img src =" https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter"/> : <img src={idData.image}/>}
       <div className="text-container-details">
@@ -24,6 +27,7 @@ function DetailedCharacter({characters}) {
       <p> House: {idData.house} </p>
       </div> 
       </div>
+      </>
     
    
   )
